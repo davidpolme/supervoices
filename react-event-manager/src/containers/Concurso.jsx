ec34c99403fd74d3b583df10d1ed65d2e9820c86
david.polania <david.polania@rappi.com>
2022-02-17 15:08:51 -0500

    fix: Dashboard - Concurso...Unfinished

--- react-event-manager/src/containers/Concurso.jsx
+++ react-event-manager/src/containers/Concurso.jsx
@@ -14,7 +14,7 @@ class Concurso extends React.Component {
     }
 
     listData(e) {
-        Axios.get('http://127.0.0.1:5000/api/locutores/<id>', {
+        Axios.get('http://127.0.0.1:5000/api/locutores', {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
