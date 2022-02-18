b26a3c3574a848408d8a92b5849e251f94d8e84a
paezfm <paezfmt@gmail.com>
2022-02-17 22:38:03 -0500

    Update Login.jsx

--- react-event-manager/src/containers/Login.jsx
+++ react-event-manager/src/containers/Login.jsx
@@ -1,29 +1,35 @@
 import React, {useState} from "react";
 import '../assets/styles/login.css';
 import {Link} from 'react-router-dom';
+import { Button } from "react-bootstrap";
+import {useNavigate} from 'react-router-dom';
 
 export const Login = () => {
 	const [email, setEmail]=useState();
 	const [clave, setClave]=useState();
+	const token = sessionStorage.getItem("token");
+	const navigate=useNavigate();
 
-	const handleClick=()=>{
+	const handleClick=(e)=>{
 		const opts={
-			method: 'POST',
+			method: "POST",
 			headers:{
-				"Content-Type":"application/json"
+				"Content-Type":"application/json",
+				'Access-Control-Allow-Origin' : '*',
 			},
 			body: JSON.stringify({
 				"email":email,
 				"clave":clave
 			})
 			
-		}
+		};
 		fetch('http://127.0.0.1:5000/api/login', opts)
-		.then(resp=>{
-			if(resp.status===200) return resp.json();
-			else  alert("No Autorizado");
+		.then(resp=>resp.json())
+		.then(data=>{
+			sessionStorage.setItem("token",data.access_token)
+
 		})
-		.then()
+		.then(navigate('/concurso'))
 		.catch(error=>{
 			console.error("There was an error", error);
 		})
@@ -35,15 +41,16 @@ export const Login = () => {
 		<section className='login'>
 		<section className='login__container'>
 			<h2>Inicia sesión</h2>
+			{token && token !=="" && token !== undefined ?("You are logged in with this token: "+token) : (
 			<form className='login__container--form'>
 				<input className='input' type='text' placeholder='Correo' value={email} onChange={(e)=> setEmail(e.target.value)}/>
 				<input className='input' type='password' placeholder='Contraseña' value={clave} onChange={(e)=> setClave(e.target.value)}/>
-				<button className='button' onClick={handleClick}>Iniciar sesión</button>
+				<Button className='button' onClick={handleClick}>Iniciar sesión</Button>
 				<div className='login__container--remember-me'>
 					<a href='/'>Olvidé mi contraseña</a>
 				</div>
 			</form>
-			
+			)}
 			<p className='login__container--register'>
 				No tienes ninguna cuenta 
 				<Link to="/register"> Regístrate</Link> 
