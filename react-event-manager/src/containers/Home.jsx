d4e373c5f48ab83560cf67008e1a1977a1d1c82c
paezfm <paezfmt@gmail.com>
2022-02-18 05:07:31 -0500

    Update Home.jsx

--- react-event-manager/src/containers/Home.jsx
+++ react-event-manager/src/containers/Home.jsx
@@ -11,9 +11,6 @@ const Home = () => {
         <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
           <h2>Bienvenidos a <span>SUPERVOICES</span></h2>
           <p>El super concurso de voces</p>
-          <div class="d-flex">
-            <a href="#contact" class="btn-get-started scrollto">CREAR CUENTA</a>
-          </div>
         </div>
       </section>
 
@@ -55,35 +52,6 @@ const Home = () => {
                 </div>
               </div>
 
-            </div>
-
-          </div>
-        </section>
-
-
-        <section id="contact" class="contact">
-          <div class="container">
-
-            <div class="section-header">
-              <h2>Formulario crear cuenta</h2>
-              <p>Diligencie este formulario para crear una cuenta con nosotros</p>
-            </div>
-
-          </div>
-
-          <div class="container">
-            <div class="col-lg-12">
-              <form action="forms/contact.php" method="post" class="php-email-form">
-                <div class="row">
-                  <div class="col-md-6 form-group">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required />
-                  </div>
-                  <div class="col-md-6 form-group mt-3 mt-md-0">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required />
-                  </div>
-                </div>
-                <div class="text-center"><button type="submit">INSCRIBIRSE</button></div>
-              </form>
             </div>
 
           </div>
