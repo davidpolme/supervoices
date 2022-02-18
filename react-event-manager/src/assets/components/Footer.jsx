4aba7991d611c9ba55703993340510349e5f44cc
Alejandro <lamedinaa@gmail.com>
2022-02-17 21:13:37 -0500

    cambio diminuto en el footer

--- react-event-manager/src/assets/components/Footer.jsx
+++ react-event-manager/src/assets/components/Footer.jsx
@@ -2,9 +2,51 @@ import React from 'react';
 
 
 const Footer = () => (
-    <div className="Footer">
-        <h1>Footer hola mundo</h1>
-    </div>
+        <footer className="page-footer font-small blue pt-4">
+            <br />
+            <br />
+            <br />
+            <br />
+
+            
+            <div className="container-fluid text-center text-md-left">
+                <div className="row">
+                    <div className="col-md-6 mt-md-0 mt-3">
+                        <h5 className="text-uppercase">SuperVoices</h5>
+                        <p>Registre sus concursos fácilmente</p>
+                    </div>
+
+                    <hr className="clearfix w-100 d-md-none pb-0"/>
+
+                    {/* <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="#!">Link 1</a></li>
+                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="#!">Link 3</a></li>
+                            <li><a href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="#!">Link 1</a></li>
+                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="#!">Link 3</a></li>
+                            <li><a href="#!">Link 4</a></li>
+                        </ul>
+                    </div> */}
+
+                    
+                </div>
+            </div>
+
+            <div className="footer-copyright text-center py-3">© 2020 Copyright:
+                <a href="https://mdbootstrap.com/"> SuperVoices.com</a>
+            </div>
+
+        </footer>
 );
 
 export default Footer;
