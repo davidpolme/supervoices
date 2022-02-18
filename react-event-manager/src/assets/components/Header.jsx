c672c7f51d96aa8d55009970c4e19f179bc9c5c1
Alejandro <lamedinaa@gmail.com>
2022-02-17 22:02:02 -0500

    navegación sencilla

--- react-event-manager/src/assets/components/Header.jsx
+++ react-event-manager/src/assets/components/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import '../styles/header.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavDropdown,Nav } from 'react-bootstrap';
 
 const Header = () => (
-    <header className="header">
-        <div className="header__menu">
-            <Link to="/register">Regístrate</Link>
-            <Link to="/login">Inicia Sesión</Link>
-            <Link to="/">Inicio</Link>
-        </div>
-    </header>
+    
+        <Navbar bg="light" expand="lg">
+               
+                <Navbar.Brand href="#home">Super voices</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                    <Nav.Link href="/register">Registrate</Nav.Link>
+                    <Nav.Link href="/login">Inicia sesión</Nav.Link>
+                    <Nav.Link href="/">Inicio</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+                <br />
+        </Navbar>
+
 
 );
 
